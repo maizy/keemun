9d39ab42d00a4f02df8f2e0fd5269eccc65354e7
Nikita Kovaliov <nikita@maizy.ru>
2013-12-13 20:11:59 +0400

    PostArguments trait, functional-style in Language.set Action :smiling_imp:

--- app/controllers/Language.scala
+++ app/controllers/Language.scala
@@ -3,24 +3,21 @@ package controllers
 import play.api.mvc.{Action, Controller}
 import play.api.i18n.Lang
 import play.api.Play.current
+import hedgehog.controller.helper.PostArguments
 
 
 /**
  * Copyright (c) Nikita Kovaliov, maizy.ru, 2013
  * See LICENSE.txt for details.
  */
-object Language extends Controller {
+object Language extends Controller with PostArguments {
 
-  def set = Action(parse.tolerantFormUrlEncoded) { implicit request =>
-    val avalableLangCodes = Lang.availables.map(_.code)
-    val maybeLang = request.body.get("lang")
-      .map(_.last)
-      .filter(avalableLangCodes.contains(_))
+  def set = Action { implicit request =>
+    val avalableLangsCodes = Lang.availables.map(_.code)
+    postArg("lang")
+      .filter(avalableLangsCodes.contains(_))
       .map(Lang(_))
-    if (maybeLang.isEmpty) {
-      BadRequest("One of those \""+ avalableLangCodes.mkString("\", \"") +"\" languages is required")
-    } else {
-      Redirect(request.headers.get(REFERER).getOrElse("/")).withLang(maybeLang.get)
-    }
+      .map(l => Redirect(request.headers.get(REFERER).getOrElse("/")).withLang(l))
+      .getOrElse(BadRequest("One of those \""+ avalableLangsCodes.mkString("\", \"") +"\" languages is required"))
   }
 }
---
+++ app/hedgehog/controller/helper/PostArguments.scala
@@ -0,0 +1,18 @@
+package hedgehog.controller.helper
+
+import play.api.mvc.{Controller, Request, AnyContent}
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2013
+ * See LICENSE.txt for details.
+ */
+trait PostArguments extends Controller {
+
+  def postArgSeq(name: String)(implicit request: Request[AnyContent]) : Option[Seq[String]] = {
+    request.body.asFormUrlEncoded.flatMap(_.get(name))
+  }
+
+  def postArg(name: String)(implicit request: Request[AnyContent]) : Option[String] = {
+    postArgSeq(name).map(_.last)
+  }
+}
