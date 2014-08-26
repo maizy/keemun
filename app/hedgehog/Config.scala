37a13ef6020d50f6523f5ea3ac8f571cf1483b62
Nikita Kovaliov <nikita@maizy.ru>
2014-08-26 20:16:05 +0400

    iss #22: custom UserAgent header

--- app/hedgehog/Config.scala
+++ app/hedgehog/Config.scala
@@ -22,7 +22,7 @@ class Config (appConfiguration: Configuration, asyncContext: ExecutionContext) {
   lazy val githubClient = new github.Client(githubClientConfig, asyncContext) //TODO: is there good place for that?
 
 
-  val version = "0.0.1"  //TODO: sync with build.sbt (generate on project builded)
+  val version = hedgehog.Version.SYMBOLIC
   val projectRepoUrl = "https://github.com/maizy/hedGeHog"
   val copyrightYears = "2013-2014"
   lazy val sources = new Sources(appConfiguration)
---
+++ app/hedgehog/Version.scala
@@ -0,0 +1,9 @@
+package hedgehog
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2014
+ * See LICENSE.txt for details.
+ */
+object Version {
+  val SYMBOLIC = "0.0.1"  //TODO: sync with build.sbt (generate on project builded)
+}
